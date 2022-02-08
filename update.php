7f9d81ad9ce5f38d14f74b080509f076fd1e4b49
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-08 16:34:39 -0700

    u

--- update.php
+++ update.php
@@ -17,12 +17,12 @@
 		<h2>Home Page</h2>
 	</div>
 	
-    <div class="nav-list">
+    <nav class="nav-list">
         <li class="nav-list-item"><a class="nav-list-link" href="update.php">Home</a></li>
         <li class="nav-list-item"><a class="nav-list-link" href=""></a></li>
         <li class="nav-list-item"><a class="nav-list-link" href=""></a></li>
         <li class="nav-list-item"><a class="nav-list-link" href="index.php">Profile Info</a></li>
-    </div>
+</nav>
 
     <div class="main-content">
         <p>
