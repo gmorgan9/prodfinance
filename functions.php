f7ba2281e0da7d338b5f1f5b64f105b235acf217
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-07 23:58:55 -0700

    upd

--- functions.php
+++ functions.php
@@ -157,3 +157,12 @@ function login(){
 		}
 	}
 }
+
+function isAdmin()
+{
+	if (isset($_SESSION['user']) && $_SESSION['user']['user_type'] == 'admin' ) {
+		return true;
+	}else{
+		return false;
+	}
+}
