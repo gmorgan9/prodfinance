a14170c12812314a24c8c6f34725252faac6769f
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-07 23:55:56 -0700

    u

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php 
 	include('functions.php');
+    if (!isLoggedIn()) {
+        $_SESSION['msg'] = "You must log in first";
+        header('location: login.php');
+    }
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
