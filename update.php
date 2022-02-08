c16b0e767d22e9fe496078ada902076e8f6f5f24
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-08 16:44:48 -0700

    u

--- update.php
+++ update.php
@@ -17,7 +17,7 @@
 		<h2>Home Page</h2>
 	</div>
 	
-    <nav class="nav-list" style="text-align:center;">
+    <nav class="nav-list" style="margin-top:10px;">
         <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="update.php">Home</a></li>
         <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href=""></a>COMPLETE</li>
         <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href=""></a></li>
