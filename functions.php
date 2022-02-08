32ed4e815348dfb695b0fd858620eb6918c1986d
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-08 16:20:46 -0700

    u

--- functions.php
+++ functions.php
@@ -151,7 +151,7 @@ function login(){
 				$_SESSION['user'] = $logged_in_user;
 				$_SESSION['success']  = "You are now logged in";
 
-				header('location: index.php');
+				header('location: update.php');
 			}
 		}else {
 			array_push($errors, "Wrong username/password combination");
