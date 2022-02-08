06c50a4bfa8a3a39cfa32b254a93eb89f818f237
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-08 00:05:47 -0700

    u

--- home.php
+++ home.php
@@ -1,15 +1,15 @@
 <?php 
-include('../functions.php');
+include('functions.php');
 
 if (!isAdmin()) {
 	$_SESSION['msg'] = "You must log in first";
-	header('location: ../login.php');
+	header('location: login.php');
 }
 
 if (isset($_GET['logout'])) {
 	session_destroy();
 	unset($_SESSION['user']);
-	header("location: ../login.php");
+	header("location: login.php");
 }
 ?>
 <!DOCTYPE html>
