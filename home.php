65a6af14b0b87e52b4e5e385a647825ba87b38b2
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-08 17:07:45 -0700

    u

--- home.php
+++ home.php
@@ -54,6 +54,22 @@ if (isset($_GET['logout'])) {
 					<small>
 						<i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i> 
 						<br>
+
+
+						<?php if(isset($_SESSION['user_type']) && !empty($_SESSION['user_type'] == 'admin') )
+							{
+									?>
+      							<li id="login"><a href="logout.php">Logout</a></li>
+<?php }else{ ?>
+     <li id="login"><a href="login.php">Login</a></li>
+     <li id="signup"><a href="register.php">Register</a></li>
+<?php } ?>
+
+
+
+
+
+
 						<a href="index.php?logout='1'"><button style="cursor:pointer;color: #ff3860;border: 1px solid #ff3860;padding:2px;border-radius: 5px;">Logout</button></a>
 					</small>
 					<a href="update.php"><button style="cursor:pointer;margin-left:5px;color: #498afb;border: 1px solid #498afb;padding:2px;border-radius: 5px;">Home</button></a>
