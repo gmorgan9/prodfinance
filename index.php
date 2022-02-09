0993595660b09234265fc298bc78dbabfb11cc41
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-08 18:52:38 -0700

    u

--- index.php
+++ index.php
@@ -14,36 +14,25 @@
 	<div class="header">
 		<h2>Home Page</h2>
 	</div>
-	
-    <!-- <nav class="nav-list" style="margin-top:10px;text-align:center;">
-        <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="update.php">Home</a></li>
-        <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href=""></a>COMPLETE</li>
-        <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href=""></a></li>
 
-
-        <li class="nav-list-item" style="list-style-type:none;display:inline;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="index.php">Profile Info</a></li>
-
-        <li class="nav-list-item" style="list-style-type:none;display:inline;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="login.php">login</a></li> -->
-
-        <?php if(isLoggedIn() )
-							{
-							?>
-      							<nav class="nav-list" style="margin-top:10px;text-align:center;">
-        <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="index.php">Home</a></li>
-        <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href=""></a>COMPLETE</li>
-        <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href=""></a></li>
-        <li class="nav-list-item" style="list-style-type:none;display:inline;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="standard.php">Profile Info</a></li>
-							<?php }else{ ?>
-								<nav class="nav-list" style="margin-top:10px;text-align:center;">
-        <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="index.php">Home</a></li>
-        <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href=""></a>COMPLETE</li>
-        <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href=""></a></li>
-        <li class="nav-list-item" style="list-style-type:none;display:inline;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="login.php">Login</a></li>
-        <li class="nav-list-item" style="list-style-type:none;display:inline;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="register.php">Register</a></li>
-							<?php } ?>
-
-
-</nav>
+        <?php if(isLoggedIn())
+		{
+		?>
+      	    <nav class="nav-list" style="margin-top:10px;text-align:center;">
+                <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="index.php">Home</a></li>
+                <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href=""></a>COMPLETE</li>
+                <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href=""></a></li>
+                <li class="nav-list-item" style="list-style-type:none;display:inline;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="standard.php">Profile Info</a></li>
+            </nav
+		<?php }else{ ?>
+			<nav class="nav-list" style="margin-top:10px;text-align:center;">
+                <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="index.php">Home</a></li>
+                <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href=""></a>COMPLETE</li>
+                <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href=""></a></li>
+                <li class="nav-list-item" style="list-style-type:none;display:inline;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="login.php">Login</a></li>
+                <li class="nav-list-item" style="list-style-type:none;display:inline;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="register.php">Register</a></li>
+            </nav>
+		<?php } ?>
 
     <div class="main-content">
         <p>
