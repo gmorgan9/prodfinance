9496c4914fea838212991a83a3909bdac14c5e49
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-08 18:56:03 -0700

    up

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
                 <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="index.php">Home</a></li>
                 <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="#">Portfolio</a></li>
                 <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="#">About</a></li>
-                <li class="nav-list-item" style="list-style-type:none;display:inline;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="standard.php">Profile</a></li>
+                <li class="nav-list-item" style="list-style-type:none;display:inline;text-align:center;margin-right:8px;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="standard.php">Profile</a></li>
                 <li><a href="index.php?logout='1'"><button style="list-style-type:none;display:inline;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link"">Logout</button></a></li>
             </nav
 		<?php }else{ ?>
@@ -30,8 +30,8 @@
                 <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="index.php">Home</a></li>
                 <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="">Portfolio</a></li>
                 <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="">About</a></li>
-                <li class="nav-list-item" style="list-style-type:none;display:inline;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="login.php">Login</a></li>
-                <li class="nav-list-item" style="list-style-type:none;display:inline;text-align:center;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="register.php">Register</a></li>
+                <li class="nav-list-item" style="list-style-type:none;display:inline;text-align:center;margin-right:8px;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="login.php">Login</a></li>
+                <li class="nav-list-item" style="list-style-type:none;display:inline;text-align:center;margin-right:8px;"><a style="color:black; text-decoration:none;" class="nav-list-link" href="register.php">Register</a></li>
             </nav>
 		<?php } ?>
 
