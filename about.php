99c734be19da60fb7bec6f19e7f104926de96fdc
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-12 14:58:20 -0700

    u

--- about.php
+++ about.php
@@ -29,6 +29,33 @@
 	        border-radius: 10px 10px 0px 0px;
 	        padding: 20px;
         }
+        /* Navigation */
+        .nav-header{
+            width: 40%;
+	        margin: 10px auto 0px;
+            color: white;
+	        background: #8fbcbc;
+	        text-align: center;
+	        border: 1px solid #B0C4DE;
+	        border-bottom: none;
+	        border-radius: 10px 10px 10px 10px;
+	        padding: 15px 5px 20px 5px;
+        }
+        .nav-list {
+            margin-top:10px;
+            text-align:center;
+        }
+        .nav-list-item {
+            list-style-type:none;
+            display:inline;
+            margin-right:8px;
+            text-align:center;
+        }
+        .nav-list-link {
+            cursor:pointer;
+            color:white;
+            text-decoration:none;
+        }
     </style>
 </head>
 <body>
@@ -36,33 +63,35 @@
 		<h2>About Page</h2>
 	</div>
 
+    <div class="nav-header">
         <?php if(isLoggedIn())
 		{
 		?>
-      	    <nav class="nav-list" style="margin-top:10px;text-align:center;">
-                <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="cursor:pointer;color:black; text-decoration:none;" class="nav-list-link" href="index.php">Home</a></li>
+      	    <nav class="nav-list">
+                <li class="nav-list-item"><a class="nav-list-link" href="index.php">Home</a></li>
                 | &nbsp;
-                <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="cursor:pointer;color:black; text-decoration:none;" class="nav-list-link" href="portfolio.php">Portfolio</a></li>
+                <li class="nav-list-item"><a class="nav-list-link" href="portfolio.php">Portfolio</a></li>
                 | &nbsp;
-                <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="cursor:pointer;color:black; text-decoration:none;" class="nav-list-link" href="about.php">About</a></li>
+                <li class="nav-list-item"><a class="nav-list-link" href="about.php">About</a></li>
                 | &nbsp;
-                <li class="nav-list-item" style="list-style-type:none;display:inline;text-align:center;margin-right:8px;"><a style="cursor:pointer;color:black; text-decoration:none;" class="nav-list-link" href="profileinfo.php">Profile</a></li>
+                <li class="nav-list-item"><a class="nav-list-link" href="profileinfo.php">Profile</a></li>
                 | &nbsp;
-                <li class="nav-list-item" style="list-style-type:none;display:inline;text-align:center;"><a style="cursor:pointer;color:black; text-decoration:none;" class="nav-list-link" href="index.php?logout='1'">Logout</a></li>
+                <li class="nav-list-item"><a class="nav-list-link" href="index.php?logout='1'">Logout</a></li>
         </nav>
 		<?php }else{ ?>
-			<nav class="nav-list" style="margin-top:10px;text-align:center;">
-                <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="cursor:pointer;color:black; text-decoration:none;" class="nav-list-link" href="index.php">Home</a></li>
+			<nav class="nav-list">
+                <li class="nav-list-item"><a class="nav-list-link" href="index.php">Home</a></li>
                 | &nbsp;
-                <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="cursor:pointer;color:black; text-decoration:none;" class="nav-list-link" href="portfolio.php">Portfolio</a></li>
+                <li class="nav-list-item"><a class="nav-list-link" href="portfolio.php">Portfolio</a></li>
                 | &nbsp;
-                <li class="nav-list-item" style="list-style-type:none;display:inline;margin-right:8px;text-align:center;"><a style="cursor:pointer;color:black; text-decoration:none;" class="nav-list-link" href="about.php">About</a></li>
+                <li class="nav-list-item"><a class="nav-list-link" href="about.php">About</a></li>
                 | &nbsp;
-                <li class="nav-list-item" style="list-style-type:none;display:inline;text-align:center;margin-right:8px;"><a style="cursor:pointer;color:black; text-decoration:none;" class="nav-list-link" href="login.php">Login</a></li>
+                <li class="nav-list-item"><a class="nav-list-link" href="login.php">Login</a></li>
                 | &nbsp;
-                <li class="nav-list-item" style="list-style-type:none;display:inline;text-align:center;margin-right:8px;"><a style="cursor:pointer;color:black; text-decoration:none;" class="nav-list-link" href="register.php">Register</a></li>
+                <li class="nav-list-item"><a class="nav-list-link" href="register.php">Register</a></li>
                 </nav>
 		<?php } ?>
+        </div>
         
 
     <div class="main-content">
