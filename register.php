48f5eaa104111fe634a2f1e2534f43e1a6a717ea
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-12 00:46:00 -0700

    u

--- register.php
+++ register.php
@@ -115,6 +115,7 @@ if (isLoggedIN()) {
 	</div>
 	<div class="input-group">
 		<button type="submit" class="btn" name="register_btn">Register</button>
+		<a href="index.php" name="btn" class="btn">←Back</a>
 	</div>
 	<p>
 		Already a member? <a href="login.php">Sign in</a>
