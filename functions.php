4233358707e5aab6f04b7e5dd8d4ba163b5cfbf6
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-12 00:37:09 -0700

    u

--- functions.php
+++ functions.php
@@ -196,7 +196,6 @@ function resetPass(){
 	// register user if there are no errors in the form
 	if (count($errors) == 0) {
 		$password = md5($password_1);//encrypt the password before saving in the database
-		$username = $_SESSION['user'];
 
 			$sql = "UPDATE users SET password = '$password' WHERE username = '$_SESSION[user][username]'";
 			mysqli_query($db, $sql);
