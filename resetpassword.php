f30706aa401952241d1d163784028b1e32375361
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-12 00:29:43 -0700

    u

--- resetpassword.php
+++ resetpassword.php
@@ -94,7 +94,7 @@
         <form method="post" action="resetpassword.php">
         <!-- <p>Please fill out this form to reset your password.</p> -->
 <?php echo display_error(); ?>
-        <p><?php print_r($_SESSION); ?></p>
+        <p><?php echo $_SESSION["loggedin"]; ?></p>
 	<div class="input-group">
 		<label>Password</label>
 		<input type="password" name="password_1">
