2b288674bc8e8864a0bfd00a1a0ac17e28609243
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-12 00:22:02 -0700

    u

--- functions.php
+++ functions.php
@@ -182,7 +182,7 @@ function resetPass(){
 
 	// receive all input values from the form. Call the e() function
     // defined below to escape form values
-	$id   = $_SESSION[id];
+	$id   = $_SESSION['id'];
 	$password_1 = e($_POST['password_1']);
 	$password_2 = e($_POST['password_2']);
 
