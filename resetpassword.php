2d4d06836d9894dc284827ff25d55dd1eac81786
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-11 22:18:43 -0700

    u

--- resetpassword.php
+++ resetpassword.php
@@ -8,7 +8,6 @@ include('functions.php');
 <head>
     <meta charset="UTF-8">
     <title>Reset Password</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
 		* { 
 	        margin: 0px; 
