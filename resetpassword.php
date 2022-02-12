b586245b191d05a36724facdf4caee8bdaf9f85a
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-12 00:12:36 -0700

    u

--- resetpassword.php
+++ resetpassword.php
@@ -2,11 +2,11 @@
 // Initialize the session
 session_start();
  
-// Check if the user is logged in, otherwise redirect to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
-    exit;
-}
+// // Check if the user is logged in, otherwise redirect to login page
+// if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+//     header("location: login.php");
+//     exit;
+// }
  
 // Include config file
 require_once "config.php";
