25181e518f01a4fde70375b79549bf2c97d8ba5e
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-11 23:04:40 -0700

    u

--- functions.php
+++ functions.php
@@ -180,7 +180,7 @@ function resetPass(){
 	// call these variables with the global keyword to make them available in function
 	global $db, $errors;
 
-	$_SESSION['user'] = $logged_in_user;
+	
 
 	// receive all input values from the form. Call the e() function
     // defined below to escape form values
@@ -198,9 +198,9 @@ function resetPass(){
 	// register user if there are no errors in the form
 	if (count($errors) == 0) {
 		$password = md5($password_1);//encrypt the password before saving in the database
-
+		$_SESSION['user'] = $logged_in_user;
 			// $query = "UPDATE users SET password = $password WHERE username = $username";
-			$query = "UPDATE users SET password = ? WHERE user = ?";
+			$query = "UPDATE users SET password = ? WHERE $_SESSION['user'] = ?";
 			mysqli_query($db, $query);
 			$_SESSION['success']  = "Password successfully updated";
 			header('location: standardprofileinfo.php');
