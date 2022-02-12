2fb25eda3141da5eb0e1f97406663b871c776c90
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-12 15:45:35 -0700

    m

--- portfolio.php
+++ portfolio.php
@@ -62,7 +62,7 @@
         }
         /* Column KB Cards */
         .row {
-            margin-left: 18%;
+            margin-left: 12%;
         }
 
         /* Add padding BETWEEN each column (if you want) */
