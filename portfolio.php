a725fba9cb743ca621da33ab34c66964b085d695
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-12 15:13:09 -0700

    u

--- portfolio.php
+++ portfolio.php
@@ -68,7 +68,7 @@
         }
         /* Remove extra left and right margins, due to padding in columns */
         .row {
-            margin: 0 -5px 0 35px;
+            margin: 0 auto 0 auto;
         }
         /* Clear floats after the columns */
         .row:after {
