33aec077213b75301139304aa7d0782e378f550a
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-12 14:32:22 -0700

    u

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
             width: 40%;
 	        margin: 0px auto 0px;
 	        color: white;
-	        background: #009296;
+	        background: #bfd9d9;
 	        text-align: center;
 	        border: 1px solid #B0C4DE;
 	        border-bottom: none;
