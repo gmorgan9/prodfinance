b3c3b32336078da0718675952e93a50cbc0b5846
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-12 16:18:00 -0700

    u

--- index.php
+++ index.php
@@ -13,43 +13,43 @@
 <body>
 	<div class="header">
 		<h2>Home Page</h2>
-	
-        
-        </div>
+    </div>
 
-        <div class="nav-header">
-        <?php if(isLoggedIn())
-		{
-		?>
-      	    <nav class="nav-list">
-                <li class="nav-list-item"><a class="nav-list-link" href="index.php">Home</a></li>
-                | &nbsp;
-                <li class="nav-list-item"><a class="nav-list-link" href="portfolio.php">Portfolio</a></li>
-                | &nbsp;
-                <li class="nav-list-item"><a class="nav-list-link" href="about.php">About</a></li>
-                | &nbsp;
-                <li class="nav-list-item"><a class="nav-list-link" href="profileinfo.php">Profile</a></li>
-                | &nbsp;
-                <li class="nav-list-item"><a class="nav-list-link" href="index.php?logout='1'">Logout</a></li>
-        </nav>
-		<?php }else{ ?>
-			<nav class="nav-list">
-                <li class="nav-list-item"><a class="nav-list-link" href="index.php">Home</a></li>
-                | &nbsp;
-                <li class="nav-list-item"><a class="nav-list-link" href="portfolio.php">Portfolio</a></li>
-                | &nbsp;
-                <li class="nav-list-item"><a class="nav-list-link" href="about.php">About</a></li>
-                | &nbsp;
-                <li class="nav-list-item"><a class="nav-list-link" href="login.php">Login</a></li>
-                | &nbsp;
-                <li class="nav-list-item"><a class="nav-list-link" href="register.php">Register</a></li>
-                </nav>
-		<?php } ?>
-        </div>
+    <div class="nav-header">
+    <?php if(isLoggedIn())
+	{
+	?>
+        <nav class="nav-list">
+            <li class="nav-list-item"><a class="nav-list-link" href="index.php">Home</a></li>
+            | &nbsp;
+            <li class="nav-list-item"><a class="nav-list-link" href="portfolio.php">Portfolio</a></li>
+            | &nbsp;
+            <li class="nav-list-item"><a class="nav-list-link" href="about.php">About</a></li>
+            | &nbsp;
+            <li class="nav-list-item"><a class="nav-list-link" href="profileinfo.php">Profile</a></li>
+            | &nbsp;
+            <li class="nav-list-item"><a class="nav-list-link" href="index.php?logout='1'">Logout</a></li>
+    </nav>
+	<?php }else{ ?>
+		<nav class="nav-list">
+            <li class="nav-list-item"><a class="nav-list-link" href="index.php">Home</a></li>
+            | &nbsp;
+            <li class="nav-list-item"><a class="nav-list-link" href="portfolio.php">Portfolio</a></li>
+            | &nbsp;
+            <li class="nav-list-item"><a class="nav-list-link" href="about.php">About</a></li>
+            | &nbsp;
+            <li class="nav-list-item"><a class="nav-list-link" href="login.php">Login</a></li>
+            | &nbsp;
+            <li class="nav-list-item"><a class="nav-list-link" href="register.php">Register</a></li>
+            </nav>
+	<?php } ?>
+    </div>
+
     <div class="main-content">
         <p>
             This is some random content!
         </p>
     </div>
+    
 </body>
 </html>
