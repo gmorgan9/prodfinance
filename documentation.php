7a4a201983d0ddcf0d27abeef275e386812d52b4
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-13 13:24:40 -0700

    m

--- documentation.php
+++ documentation.php
@@ -188,9 +188,9 @@ LOGO
             <li class="nav-list-item"><a class="nav-list-link" href="documentation.php">Documentation</a></li>
             | &nbsp;
             <li class="nav-list-item"><a class="nav-list-link" href="about.php">About</a></li>
-            <li class="nav-list-item" style="text-align:right;"><a class="nav-list-link" href="profileinfo.php">Profile</a></li>
+            <li class="nav-list-item" style="float:right;"><a class="nav-list-link" href="profileinfo.php">Profile</a></li>
             | &nbsp;
-            <li class="nav-list-item" style="text-align:right;"><a class="nav-list-link" href="index.php?logout='1'">Logout</a></li>
+            <li class="nav-list-item"><a class="nav-list-link" href="index.php?logout='1'">Logout</a></li>
         </nav>
 		<?php }else{ ?>
 			<nav class="nav-list">
@@ -199,9 +199,9 @@ LOGO
           <li class="nav-list-item"><a class="nav-list-link" href="documentation.php">Documentation</a></li>
           | &nbsp;
           <li class="nav-list-item"><a class="nav-list-link" href="about.php">About</a></li>
-          <li class="nav-list-item" style="text-align:right;"><a class="nav-list-link" href="login.php">Login</a></li>
+          <li class="nav-list-item"><a class="nav-list-link" href="login.php">Login</a></li>
           | &nbsp;
-          <li class="nav-list-item" style="text-align:right;"><a class="nav-list-link" href="register.php">Register</a></li>
+          <li class="nav-list-item"><a class="nav-list-link" href="register.php">Register</a></li>
       </nav>
 		<?php } ?>
   </div>
