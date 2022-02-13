9ac88b30dd54dd214cc02bed362604ded021547f
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-13 13:49:45 -0700

    m

--- login.php
+++ login.php
@@ -121,7 +121,7 @@ if (isLoggedIN()) {
 		<div class="input-group">
 			<button type="submit" class="btn" name="login_btn">Login</button>
 			<a href="index.php" name="btn" class="btn">Back</a>
-			<input type="button" value="Go back!" onclick="history.back()">
+			<input type="button" name="btn" class="btn" value="Back" onclick="history.back()">
 		</div>
 		<p>
 			Not yet a member? <a href="register.php">Sign up</a>
