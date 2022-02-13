8ce148fa7d2ae6dee64f0fa4bc1fc094838acaa1
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-13 13:18:00 -0700

    n

--- documentation.php
+++ documentation.php
@@ -178,42 +178,42 @@ LOGO
 		<h2>Documentation</h2>
 	</div>
 
-    <div class="nav-header">
-        <?php if(isLoggedIn())
+  <div class="nav-header">
+    <?php if(isLoggedIn())
 		{
 		?>
-      	    <nav class="nav-list">
-              <div class="nav-step">
-                <li class="nav-list-item"><a class="nav-list-link" href="index.php">Home</a></li>
-                | &nbsp;
-                <li class="nav-list-item"><a class="nav-list-link" href="documentation.php">Documentation</a></li>
-                | &nbsp;
-                <li class="nav-list-item"><a class="nav-list-link" href="about.php">About</a></li>
-                </div>
-                <div class="log-step">
-                <li class="nav-list-item"><a class="nav-list-link" href="profileinfo.php">Profile</a></li>
-                | &nbsp;
-                <li class="nav-list-item"><a class="nav-list-link" href="index.php?logout='1'">Logout</a></li>
-                </div>
+        <nav class="nav-list">
+          <div class="nav-step">
+            <li class="nav-list-item"><a class="nav-list-link" href="index.php">Home</a></li>
+            | &nbsp;
+            <li class="nav-list-item"><a class="nav-list-link" href="documentation.php">Documentation</a></li>
+            | &nbsp;
+            <li class="nav-list-item"><a class="nav-list-link" href="about.php">About</a></li>
+          </div>
+          <div class="log-step">
+            <li class="nav-list-item"><a class="nav-list-link" href="profileinfo.php">Profile</a></li>
+            | &nbsp;
+            <li class="nav-list-item"><a class="nav-list-link" href="index.php?logout='1'">Logout</a></li>
+          </div>
         </nav>
 		<?php }else{ ?>
 			<nav class="nav-list">
         <div class="nav-step">
-                <li class="nav-list-item"><a class="nav-list-link" href="index.php">Home</a></li>
-                | &nbsp;
-                <li class="nav-list-item"><a class="nav-list-link" href="documentation.php">Documentation</a></li>
-                | &nbsp;
-                <li class="nav-list-item"><a class="nav-list-link" href="about.php">About</a></li>
-                </div>
-                <div class="log-step">
-                <li class="nav-list-item"><a class="nav-list-link" href="login.php">Login</a></li>
-                | &nbsp;
-                <li class="nav-list-item"><a class="nav-list-link" href="register.php">Register</a></li>
-                </div>
-                </nav>
-		<?php } ?>
+          <li class="nav-list-item"><a class="nav-list-link" href="index.php">Home</a></li>
+          | &nbsp;
+          <li class="nav-list-item"><a class="nav-list-link" href="documentation.php">Documentation</a></li>
+          | &nbsp;
+          <li class="nav-list-item"><a class="nav-list-link" href="about.php">About</a></li>
         </div>
-        <br><br>
+        <div class="log-step">
+          <li class="nav-list-item"><a class="nav-list-link" href="login.php">Login</a></li>
+          | &nbsp;
+          <li class="nav-list-item"><a class="nav-list-link" href="register.php">Register</a></li>
+        </div>
+      </nav>
+		<?php } ?>
+  </div>
+        <br><br><br>
 
     <div class="main-content">
       <div class="content-header">
