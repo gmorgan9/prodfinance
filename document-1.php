eb81ff9c3f89c14b53e157408ed94be7851d00ed
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-13 22:38:26 -0700

    m

--- document-1.php
+++ document-1.php
@@ -178,6 +178,14 @@
         code{
             color: red;
         }
+        pre {
+  overflow-x: auto;
+  white-space: pre-wrap;
+  white-space: -moz-pre-wrap;
+  white-space: -pre-wrap;
+  white-space: -o-pre-wrap;
+  word-wrap: break-word;
+}
 
     </style>
 </head>
