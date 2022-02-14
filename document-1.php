98508bb607057d72e9c384f541e7647e63ec6c63
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-13 23:03:56 -0700

    m

--- document-1.php
+++ document-1.php
@@ -162,10 +162,14 @@
             margin-top: -50px;
             position: absolute;
           }
+          hr {
+            display: block;
+            margin-left: 75px;
+          }
         }
 
         /* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
-        @media screen and (max-width: 600px) {
+        @media screen and (max-width: 600px) {        
           .column {
             width: 100%;
             margin-left: -15px;
