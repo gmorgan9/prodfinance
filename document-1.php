77a92e87ecb174bccb9723b926258ca5d1922130
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-13 22:49:34 -0700

    m

--- document-1.php
+++ document-1.php
@@ -138,7 +138,7 @@
           }
           .steps {
             clear: all;
-            margin-left: none;
+            margin-left: none !important;
             margin-right: none !important;
             width: 100% !important;
             text-align: center !important;
