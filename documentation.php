6bfd39ef61a9c6c5243c51428a46f5e1ce5bc72e
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-15 07:59:01 -0700

    m

--- documentation.php
+++ documentation.php
@@ -8,8 +8,121 @@
 <head>
 	<title>Documentation</title>
   <meta name="viewport" content="width=device-width">
-  <link rel="stylesheet" href="styles.css?v=<?php echo time(); ?>">
     <!-- <link rel="icon" type="image/x-icon" href="logo.png"> -->
+    <style>
+       
+        
+        /* Column KB Cards */
+        .row {
+            margin-left: 8%;
+        }
+        /* Add padding BETWEEN each column (if you want) */
+        .row,
+        .row > .column {
+          padding: 8px;
+        }
+
+        /* Create four equal columns that floats next to each other */
+        .column {
+            float: left;
+            width: 30%;
+        }
+
+        /* Clear floats after rows */
+        .row:after {
+          content: "";
+          display: table;
+          clear: both;
+        }
+
+        /* Content */
+        .box {
+          background-color: #fff;
+          text-align: center;
+          height: 200px;
+          box-shadow: 4px 2px 2px rgba(170, 170, 170, 0.45);
+        }
+        .box:hover {
+          transition: transform .3s ease;
+          transform: scale(1.025);
+        }
+        .top-bar {
+            height: 20px;
+            background-size: cover;
+            background-position: 50%;
+            background-color: #002e5d;
+            /* border-radius: 10px 10px 0px 0px; */
+        }
+        .content {
+            padding: 10px;
+        }
+        .content p {
+          margin-left: 10px;
+          margin-right: 10px;
+        }
+        hr {
+          display: none;
+        }
+
+        /* Responsive layout - makes a two column-layout instead of four columns */
+        @media screen and (max-width: 900px) {
+            .header {
+            width: 350px;
+          }
+          .nav-header {
+            width: 350px;
+            height: 100px;
+          }
+          #nav-list {
+            margin-left: -2.5%;
+            margin-top: 10px;
+            margin-bottom: 5px;
+            text-align: center;
+          }
+          #login, #register, #logout, #register {
+            text-decoration: none;
+            color: white;
+          }
+          #loginreg, #profilelogout {
+            float: none;
+            margin-top: 5px;
+            text-align: center;
+          }
+          .column {
+            width: 50%;
+          }
+          .steps {
+            clear: all;
+            margin-left: 5px !important;
+            margin-right: 5px !important;
+            width: 95% !important;
+            text-align: center !important;
+          }
+          .content-header {
+            margin-left: 2.5%;
+            margin-right: 2.5%;
+          }
+          .btn {
+            margin-left: 42.5%;
+            margin-top: -50px;
+            position: absolute;
+          }
+          hr {
+            display: block;
+            margin-left: 75px;
+          }
+        }
+
+        /* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
+        @media screen and (max-width: 600px) {
+          .column {
+            width: 100%;
+            margin-left: -15px;
+          }
+        }
+
+
+    </style>
 </head>
 <body>
 
