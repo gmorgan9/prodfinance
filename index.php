2571788e02e502ba500589bd55331a0743fc09cf
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-18 08:32:36 -0700

    m

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
       <?php if(isLoggedIn()){?>
         <div id="profilelogout">
           <li class="nav-list-item"><a id="profile" href="profileinfo.php">Profile</a></li>
-          <li class="nav-list-item"><a id="logout" href="/?loggedin='1'&id=<?php echo $post['users']['id'] ?>">Logout</a></li>
+          <li class="nav-list-item"><a id="logout" href="/?logout='1'">Logout</a></li>
         </div>
 		  <?php }else{ ?>
         <div id="loginreg">
