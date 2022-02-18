12f9e7285bd3e0dc5ce5445a575b17929c233e29
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-18 12:20:31 -0700

    m

--- documentation.php
+++ documentation.php
@@ -37,12 +37,13 @@
         </div>
 		  <?php } ?>
     </div>
+    <a href="javascript:history.back()" name="btn" class="manage-btn" >Back</a>
+        <a href="create_post.php" name="btn" class="manage-btn" >Create Post</a>
+        <a href="#" name="btn" class="manage-btn" >Manage Posts</a>
   </div>
   <br><br><br>
       <div class="manage-btn-group">
-        <a href="javascript:history.back()" name="btn" class="manage-btn" >Back</a>
-        <a href="create_post.php" name="btn" class="manage-btn" >Create Post</a>
-        <a href="#" name="btn" class="manage-btn" >Manage Posts</a>
+        
       </div>
 
 
