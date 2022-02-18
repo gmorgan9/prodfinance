06361be6d6d184d0b6613034a9aa327088a0ccbd
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-18 11:26:26 -0700

    m

--- testdocument.php
+++ testdocument.php
@@ -11,14 +11,16 @@
   <link rel="stylesheet" href="styles.css?v=<?php echo time(); ?>">
   <link rel="icon" type="image/x-icon" href="fav.png">
 
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-
+  <link rel="stylesheet" href="MarkItUp/markitup/skins/simple/style.css" />
+<link rel="stylesheet" href="MarkItUp/markitup/sets/default/style.css" />
+  
 </head>
 <body>
 
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script> <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script> <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
+<script src="http://code.jquery.com/jquery-1.8.0.min.js"></script>
+<script src="MarkItUp/markitup/jquery.markitup.js"></script>
+<script src="MarkItUp/markitup/sets/default/set.js"></script>
 
 	<div class="header">
 		<h2><span style="color:gray;">Test -</span> Documentation</h2>
@@ -70,27 +72,19 @@
 		<a href="javascript:history.back()" name="btn" class="log-btn">Back</a>
 	</div>
 </form>
-
-
-<div class="container mt-4 mb-4">
-  <div class="row justify-content-md-center">
-    <div class="col-md-12 col-lg-8">
-      <h1 class="h2 mb-4">Submit issue</h1>
-      <label>Describe the issue in detail</label>
-      <div class="form-group">
-         <textarea id="editor"></textarea>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </div>
-  </div>
-</div>
    
 
 
 
     </div>
 
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script> <script> tinymce.init({ selector: 'textarea#editor', menubar: false }); </script>
+
+
+    <script>
+$(document).ready(function() {
+$('#markItUp').markItUp(mySettings);
+});
+</script>
     
 </body>
 </html>
