ae4424f075125f20e6c877e1f15da81a6fb62750
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-21 16:44:20 -0700

    m

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
     <hr style="width:50%">
     <?php if(isLoggedIn()){?>
     <div class="dropdown">
+      <img src="profile.png">
       <li class="nav-list-item" id="profile">Profile</li>
       <div class="dropdown-content">
         <li class="nav-list-item"><a id="profile" href="profileinfo.php">Dashboard</a></li>
