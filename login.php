78faf55af94bf9c51a08121b87d32606f4c56ee6
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-21 15:34:14 -0700

    m

--- login.php
+++ login.php
@@ -55,8 +55,32 @@ if (isLoggedIN()) {
 </div>
 
 <div id="Register" class="tabcontent">
-  <h3>Paris</h3>
-  <p>Paris is the capital of France.</p>
+<form method="post" action="register.php">
+<?php echo display_error(); ?>
+	<div class="input-group">
+		<label>Username</label>
+		<input type="text" name="username" value="<?php echo $username; ?>">
+	</div>
+	<div class="input-group">
+		<label>Email</label>
+		<input type="email" name="email" value="<?php echo $email; ?>">
+	</div>
+	<div class="input-group">
+		<label>Password</label>
+		<input type="password" name="password_1">
+	</div>
+	<div class="input-group">
+		<label>Confirm password</label>
+		<input type="password" name="password_2">
+	</div>
+	<div class="input-group">
+		<button type="submit" class="log-btn" name="register_btn">Register</button>
+		<a href="javascript:history.back()" name="btn" class="log-btn">Back</a>
+	</div>
+	<p>
+		Already a member? <a href="login.php">Sign in</a>
+	</p>
+</form>
 </div>
 
 </div>
