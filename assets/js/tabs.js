b00cac5eb44bba7a34d4a45f41e5153c6b3d8671
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-21 15:47:15 -0700

    m

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -1,6 +1,6 @@
 function accountTabs(evt, tabName) {
     // Declare all variables
-    var i, tabcontent, log-header-sections;
+    var i, tabcontent, tablinks;
   
     // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -9,9 +9,9 @@ function accountTabs(evt, tabName) {
     }
   
     // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("log-header-sections");
+    tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = log-header-sections[i].className.replace(" active", "");
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
   
     // Show the current tab, and add an "active" class to the button that opened the tab
