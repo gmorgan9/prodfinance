2c7137fec969e4fbedb785e3732fa1fe9b246b5f
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-21 19:07:58 -0700

    m

---
+++ app/includes/adminHeader.php
@@ -0,0 +1,20 @@
+<header>
+    <a class="logo" href="/">
+        <h1 class="logo-text"><span>Garrett</span>Inspires</h1>
+    </a>
+    <i class="fa fa-bars menu-toggle"></i>
+    <ul class="nav">
+        <?php  if (isset($_SESSION['user'])) : ?>
+            <li>
+                <a href="#">
+                    <i class="fa fa-user"></i>
+                    <?php echo $_SESSION['user']['username']; ?>
+                    <i class="fa fa-chevron-down" style="font-size: .8em;"></i>
+                </a>
+                <ul>
+                    <li><a href="/?logout='1'" class="logout">Logout</a></li>
+                </ul>
+            </li>
+        <?php endif; ?>
+    </ul>
+</header>
