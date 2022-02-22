2e8a34ffad92e9d05fa7aae536171cfa23a2c54e
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-21 18:34:16 -0700

    m

--- register.php
+++ register.php
@@ -24,23 +24,23 @@ if (isLoggedIN()) {
 <h2 class="form-title">Register</h2>
 <?php echo display_error(); ?>
 
-	<div class="input-group">
+	<div>
 		<label>Username</label>
-		<input type="text" name="username" value="<?php echo $username; ?>">
+		<input class="text-input" type="text" name="username" value="<?php echo $username; ?>">
 	</div>
-	<div class="input-group">
+	<div>
 		<label>Email</label>
-		<input type="email" name="email" value="<?php echo $email; ?>">
+		<input class="text-input" type="email" name="email" value="<?php echo $email; ?>">
 	</div>
-	<div class="input-group">
+	<div>
 		<label>Password</label>
-		<input type="password" name="password_1">
+		<input class="text-input" type="password" name="password_1">
 	</div>
-	<div class="input-group">
+	<div>
 		<label>Confirm password</label>
-		<input type="password" name="password_2">
+		<input class="text-input" type="password" name="password_2">
 	</div>
-	<div class="input-group">
+	<div>
 		<button type="submit" class="log-btn" name="register_btn">Register</button>
 		<a href="javascript:history.back()" name="btn" class="log-btn">Back</a>
 	</div>
