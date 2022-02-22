d1cdcfc44b1ed97a1f60cde7e59c2b25844c5616
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-21 20:28:29 -0700

    m

---
+++ about.php
@@ -0,0 +1,46 @@
+<?php include('functions.php'); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About</title>
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="styles.css?v=<?php echo time(); ?>">
+    <link rel="icon" type="image/x-icon" href="fav.png">
+</head>
+<body>
+	<div class="header">
+		<h2>About Page</h2>
+	</div>
+
+    
+    <div class="nav-header">
+    <div id="nav-list">
+      <li class="nav-list-item"><a class="nav-list-link" href="/">Home</a></li>
+      <li class="nav-list-item"><a class="nav-list-link" href="documentation.php">Documentation</a></li>
+      <li class="nav-list-item"><a class="nav-list-link" href="about.php">About</a></li>
+    </div>
+    <hr style="width:50%">
+    <div class="nav-list-log">
+      <?php if(isLoggedIn()){?>
+        <div id="profilelogout">
+          <li class="nav-list-item"><a id="profile" href="profileinfo.php">Profile</a></li>
+          <li class="nav-list-item"><a id="logout" href="/?logout='1'">Logout</a></li>
+        </div>
+		  <?php }else{ ?>
+        <div id="loginreg">
+          <li class="nav-list-item"><a id="login" href="login.php">Login</a></li>
+          <li class="nav-list-item"><a id="register" href="register.php">Register</a></li>
+        </div>
+		  <?php } ?>
+    </div>
+  </div>
+        
+
+    <div class="main-content">
+        <p>
+            About page
+        </p>
+    </div>
+</body>
+</html>
