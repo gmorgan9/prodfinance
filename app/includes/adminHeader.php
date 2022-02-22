d121137acc7c0baa0837b1f9b45dbce90ea3aad3
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-21 21:07:15 -0700

    m

--- app/includes/adminHeader.php
+++ app/includes/adminHeader.php
@@ -7,6 +7,8 @@
         
         <?php  if (isset($_SESSION['user'])) : ?>
             <li>
+            <i class="fa-solid fa-globe"></i>
+            <li><a href="<?php echo BASE_URL . '/'; ?>" class="logout">Logout</a></li>
                 <a href="#">
                     <i class="fa fa-user"></i>
                     <?php echo $_SESSION['user']['username']; ?>
