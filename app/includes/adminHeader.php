ab2eb1f5bd3b86a11749d78dd3db396bad7199b0
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-21 20:41:34 -0700

    m

--- app/includes/adminHeader.php
+++ app/includes/adminHeader.php
@@ -11,15 +11,21 @@
 
       <?php  if (isset($_SESSION['user'])) : ?>
         <li>
-                <a href="#">
-                    <i class="fa fa-user"></i>
-                    <?php echo $_SESSION['user']['username']; ?>
-                    <i class="fa fa-chevron-down" style="font-size: .8em;"></i>
-                </a>
-                <ul>
-                    <li><a href="<?php echo BASE_URL . '/logout.php'; ?>" class="logout">Logout</a></li>
-                </ul>
-            </li>
+          <a href="#">
+            <i class="fa fa-user"></i>
+            <?php echo $_SESSION['user']['username']; ?>
+            <i class="fa fa-chevron-down" style="font-size: .8em;"></i>
+          </a>
+          <ul>
+            <?php if($_SESSION['user']['user_type'] == 'admin' || $_SESSION['user']['user_type'] == 'superadmin'): ?>
+              <li><a href="admin/dashboard.php">Dashboard</a></li>
+            <?php endif; ?>
+            <li><a href="/?logout='1'" class="logout">Logout</a></li>
+          </ul>
+        </li>
+      <?php else: ?>
+        <li><a href="register.php">Sign Up</a></li>
+        <li><a href="login.php">Login</a></li>
       <?php endif; ?>
     </ul>
 </header>
