f5b888ada426d839c029dabcd507728973cae649
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-21 19:30:58 -0700

    m

--- index.php
+++ index.php
@@ -12,6 +12,9 @@
     <!-- Font Awesome -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
     integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Candal|Lora" rel="stylesheet">
 </head>
 <body>
 
