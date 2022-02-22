8c54ff0fce67d431ebb190d92ab5d3dac6dffa1f
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-21 19:12:51 -0700

    m

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -5,9 +5,13 @@
 <head>
 	<title>Home</title>
     <meta name="viewport" content="width=device-width">
-    <link rel="stylesheet" href="assets/css/style.css?v=<?php echo time(); ?>">
-    <!-- <link rel="stylesheet" href="styles.css"> -->
     <link rel="icon" type="image/x-icon" href="fav.png">
+
+    <!-- Custom Styling -->
+    <link rel="stylesheet" href="assets/css/style.css?v=<?php echo time(); ?>">  
+
+    <!-- Admin Styling -->
+    <link rel="stylesheet" href="../assets/css/admin.css?v=<?php echo time(); ?>">
 
     <!-- Font Awesome -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
