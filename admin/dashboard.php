c23964683ea991bc682b8eba3bafd23d4c2a30db
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-21 19:10:44 -0700

    m

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,1 +1,46 @@
-text here
+<?php include('functions.php'); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="assets/css/style.css?v=<?php echo time(); ?>">
+    <!-- <link rel="stylesheet" href="styles.css"> -->
+    <link rel="icon" type="image/x-icon" href="fav.png">
+
+    <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+</head>
+<body>
+
+<?php include("app/includes/adminHeader.php") ?>
+
+<!-- Admin Page Wrapper -->
+<div class="admin-wrapper">
+
+<?php include("app/includes/adminSidebar.php") ?>
+        
+<!-- Admin Content -->
+<div class="admin-content">
+
+<div class="content">
+
+    <h2 class="page-title">Dashboard</h2>
+
+    <?php include(ROOT_PATH . '/app/includes/messages.php'); ?>
+
+    
+
+</div>
+
+</div>
+<!-- // Admin Content -->
+
+</div>
+<!-- // Page Wrapper -->
+
+
+</body>
+</html>
