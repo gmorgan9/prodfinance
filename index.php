ae1a37ff9db7d0e3ff2574023f1e9965b88571d3
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-21 18:20:59 -0700

    m

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
     <link rel="stylesheet" href="assets/css/style.css?v=<?php echo time(); ?>">
     <!-- <link rel="stylesheet" href="styles.css"> -->
     <link rel="icon" type="image/x-icon" href="fav.png">
+
+    <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 </head>
 <body>
 
