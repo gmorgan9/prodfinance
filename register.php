bf59990b911ab84db9a2affe7cec4f75ddd121d1
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-21 18:57:44 -0700

    m

--- register.php
+++ register.php
@@ -43,7 +43,7 @@ if (isLoggedIN()) {
 	</div>
 	<div>
 		<button type="submit" class="log-btn" name="register_btn">Register</button>
-		<button name="btn" class="log-btn"><a href="javascript:history.back()"></button>Back</a>
+		<button name="btn" class="log-btn"><a href="javascript:history.back()">Back</a></button>
 	</div>
 	<p>
 		Already a member? <a href="login.php">Sign in</a>
