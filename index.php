47af31d508c1fd204f3269f397e7b2bb29537b30
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-21 17:16:52 -0700

    m

--- index.php
+++ index.php
@@ -9,30 +9,8 @@
     <link rel="icon" type="image/x-icon" href="fav.png">
 </head>
 <body>
-<div class="header">
-  <div class="logo">
-		<h2>Home Page</h2>
-  </div>
-  <div class="nav-header">
-      <li class="nav-list-item"><a class="nav-list-link" href="/">Home</a></li>
-      <li class="nav-list-item"><a class="nav-list-link" href="documentation.php">Documentation</a></li>
-      <li class="nav-list-item"><a class="nav-list-link" href="about.php">About</a></li>
-    <hr style="width:50%">
-    <?php if(isLoggedIn()){?>
-    <div class="dropdown">
-    <li class="nav-list-item" id="profile">&nbsp; &nbsp;Profile</li>
-      <div class="dropdown-content">
-        <li class="nav-list-item"><a id="profile" href="profileinfo.php">Dashboard</a></li>
-        <li class="nav-list-item"><a id="logout" href="/?logout='1'">Logout</a></li>
-      </div>
-    </div>
-		<?php }else{ ?>
-    <div id="loginreg">
-      <li class="nav-list-item"><a id="login" href="account.php">Account</a></li>
-    </div>
-		<?php } ?>
-</div>     
-</div>
+
+<?php include("app/includes/header.php") ?>
         
 
     <div class="main-content">
