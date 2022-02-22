98e9060d1f6e243b280d2da7e7a47f34611399df
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-21 18:59:04 -0700

    m

--- login.php
+++ login.php
@@ -34,7 +34,7 @@ if (isLoggedIN()) {
 </div>
 <div>
 	<button type="submit" class="log-btn" name="login_btn">Login</button>
-	<button name="btn" class="log-btn"><a href="javascript:history.back()">Back</a></button>
+	<button name="btn" class="log-btn" href="javascript:history.back()">Back</button>
 </div>
 <p>
 	Not yet a member? <a href="register.php">Sign up</a>
