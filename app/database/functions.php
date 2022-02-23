697c20bd360da266ba2486ac92d8898d7174c83f
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-22 21:15:23 -0700

    m

--- app/database/functions.php
+++ app/database/functions.php
@@ -269,3 +269,72 @@ function resetPass(){
 			header('location: profileinfo.php');		
 		}
 	}
+
+
+// SELECT ALL
+	function selectAll($table, $conditions = [])
+	{
+		global $conn;
+		$sql = "SELECT * FROM $table";
+		if (empty($conditions)) {
+			$stmt = $conn->prepare($sql);
+			$stmt->execute();
+			$records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+			return $records;
+		} else {
+			$i = 0;
+			foreach ($conditions as $key => $value) {
+				if ($i === 0) {
+					$sql = $sql . " WHERE $key=?";
+				} else {
+					$sql = $sql . " AND $key=?";
+				}
+				$i++;
+			}
+			
+			$stmt = executeQuery($sql, $conditions);
+			$records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+			return $records;
+		}
+	}
+// SELECT ONE
+function selectOne($table, $conditions)
+{
+    global $conn;
+    $sql = "SELECT * FROM $table";
+
+    $i = 0;
+    foreach ($conditions as $key => $value) {
+        if ($i === 0) {
+            $sql = $sql . " WHERE $key=?";
+        } else {
+            $sql = $sql . " AND $key=?";
+        }
+        $i++;
+    }
+
+    $sql = $sql . " LIMIT 1";
+    $stmt = executeQuery($sql, $conditions);
+    $records = $stmt->get_result()->fetch_assoc();
+    return $records;
+}
+// CREATE
+function create($table, $data)
+{
+    global $conn;
+    $sql = "INSERT INTO $table SET ";
+
+    $i = 0;
+    foreach ($data as $key => $value) {
+        if ($i === 0) {
+            $sql = $sql . " $key=?";
+        } else {
+            $sql = $sql . ", $key=?";
+        }
+        $i++;
+    }
+    
+    $stmt = executeQuery($sql, $data);
+    $id = $stmt->insert_id;
+    return $id;
+}
