2f7dc4054d511563872034cdc59fcfed22dbf1b1
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-23 00:32:07 -0700

    m

--- login.php
+++ login.php
@@ -1,6 +1,5 @@
-<?php include("path.php"); ?>
-<?php 
-include('app/database/db.php'); 
+<?php include('path.php'); ?>
+<?php include(ROOT_PATH . "/app/controllers/users.php"); 
 guestsOnly();
 ?>
 
