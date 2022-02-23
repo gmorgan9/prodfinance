02e590b40a849db9eae2804c7845aa26398f36b0
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-22 21:26:33 -0700

    m

--- app/controllers/posts.php
+++ app/controllers/posts.php
@@ -28,7 +28,7 @@ if (isset($_GET['id'])) {
 }
 
 if (isset($_GET['delete_id'])) {
-    adminOnly();
+    // adminOnly();
     $count = delete($table, $_GET['delete_id']);
     $_SESSION['message'] = "Post deleted successfully";
     $_SESSION['type'] = "success";
@@ -37,7 +37,7 @@ if (isset($_GET['delete_id'])) {
 }
 
 if (isset($_GET['published']) && isset($_GET['p_id'])) {
-    adminOnly();
+    // adminOnly();
     $published = $_GET['published'];
     $p_id = $_GET['p_id'];
     $count = update($table, $p_id, ['published' => $published]);
@@ -50,7 +50,7 @@ if (isset($_GET['published']) && isset($_GET['p_id'])) {
 
 
 if (isset($_POST['add-post'])) {
-    adminOnly();
+    // adminOnly();
     $errors = validatePost($_POST);
 
     if (!empty($_FILES['image']['name'])) {
@@ -88,7 +88,7 @@ if (isset($_POST['add-post'])) {
 
 
 if (isset($_POST['update-post'])) {
-    adminOnly();
+    // adminOnly();
     $errors = validatePost($_POST);
 
     if (!empty($_FILES['image']['name'])) {
