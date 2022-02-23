214f233c5816edd0dbf02bba7b206144309dfd09
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-23 02:05:40 -0700

    m

--- app/includes/header.php
+++ app/includes/header.php
@@ -4,9 +4,9 @@
     </a>
     <i class="fa fa-bars menu-toggle"></i>
     <ul class="nav">
-      <li><a href="<?php echo BASE_URL . '/index.php' ?>">Home</a></li>
+      <li><a href="<?php echo BASE_URL . '/' ?>">Home</a></li>
       <li><a href="#">About</a></li>
-      <li><a href="#">Services</a></li>
+      <li><a href="<?php echo BASE_URL . '/documentation_test.php' ?>">Documentation</a></li>
 
       <?php if (isset($_SESSION['id'])): ?>
         <li>
