d64491fd7a7ba5315eba80e4fa3c5944ac25c1a6
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-23 01:53:53 -0700

    m

--- admin/posts/index.php
+++ admin/posts/index.php
@@ -56,7 +56,8 @@ session_start();
                     <h2 class="page-title">Manage Posts</h2>
 
                     <?php include(ROOT_PATH . "/app/includes/messages.php"); ?>
-                    <?php echo "hi"; ?>
+                    <?php $authorname = get_the_author_meta('nickname'); ?>
+                    <?php echo $authorname; ?>
                     <table>
                         <thead>
                             <th>SN</th>
