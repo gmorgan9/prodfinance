f8b8dd28b39d566a12746d985fbe5bb21511a284
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-23 01:29:58 -0700

    m

--- index.php
+++ index.php
@@ -11,6 +11,9 @@ session_start();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+  <!-- Favicon -->
+  <link rel="icon" type="image/x-icon" href="/assets/images/fav.png">
+
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
     integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
