e1a1cc26972d8026bc68b150e5cd1fffdaa83b51
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-22 22:19:25 -0700

    m

--- app/includes/messages.php
+++ app/includes/messages.php
@@ -1,6 +1,6 @@
 <?php if(isset($_SESSION['message'])): ?>
   <div class="msg <?php echo $_SESSION['type']; ?>">
-    <li><?php echo $_SESSION['message'], $result; ?></li>
+    <li><?php echo $_SESSION['message']; ?></li>
     <?php
       unset($_SESSION['message']);
       unset($_SESSION['type']);
