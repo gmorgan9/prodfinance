84e4019fbe836a7010e9dceae68b3406ac150104
Garrett Morgan <gar.mor.pro@outlook.com>
2022-02-23 01:36:59 -0700

    m

--- app/includes/adminHeader.php
+++ app/includes/adminHeader.php
@@ -1,20 +1,14 @@
 <header>
-    <a class="logo" href="/">
-        <h1 class="logo-text"><span>Garrett</span>Inspires</h1>
+    <a class="logo" href="<?php echo BASE_URL . '/index.php'; ?>">
+        <h1 class="logo-text"><span>Awa</span>Inspires</h1>
     </a>
     <i class="fa fa-bars menu-toggle"></i>
     <ul class="nav">
-        
-        <?php  if (isset($_SESSION['user'])) : ?>
-            <li>
-            <a href="/">
-            <i class="fa fa-globe"></i>
-            Public</a>
-            </li>
+        <?php if (isset($_SESSION['username'])): ?>
             <li>
                 <a href="#">
                     <i class="fa fa-user"></i>
-                    <?php echo $_SESSION['user']['username']; ?>
+                    <?php echo $_SESSION['username']; ?>
                     <i class="fa fa-chevron-down" style="font-size: .8em;"></i>
                 </a>
                 <ul>
