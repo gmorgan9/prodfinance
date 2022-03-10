ab52312194498e49a697eb57ca412bf374f7fdeb
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-09 21:27:38 -0700

    m

--- app/controllers/expenses.php
+++ app/controllers/expenses.php
@@ -24,12 +24,6 @@ if (isset($_POST['add-expense'])) {
 
     if (count($errors) === 0) {
         unset($_POST['add-expense']);
-        $name = $_POST['name'];
-        $amount = $_POST['amount'];
-        $category = $_POST['category'];
-        $account = $_POST['account'];
-        $date = $_POST['date'];
-        $description = $_POST['description'];
         $expense_id = create($table, $_POST);
         $_SESSION['message'] = "Expense created successfully";
         $_SESSION['type'] = "success";
@@ -50,38 +44,46 @@ if (isset($_POST['add-expense'])) {
 
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
-    $topic = selectOne($table, ['id' => $id]);
-    $id = $topic['id'];
-    $name = $topic['name'];
-    $description = $topic['description'];
+    $expenses = selectOne($table, ['id' => $id]);
+    $id = $expenses['id'];
+    $name = $expenses['name'];
+    $amount = $expenses['amount'];
+    $category = $expenses['category'];
+    $account = $expenses['account'];
+    $date = $expenses['date'];
+    $description = $expenses['description'];
 }
 
 if (isset($_GET['del_id'])) {
     adminOnly();
     $id = $_GET['del_id'];
     $count = delete($table, $id);
-    $_SESSION['message'] = 'Topic deleted successfully';
+    $_SESSION['message'] = 'Expenses deleted successfully';
     $_SESSION['type'] = 'success';
-    header('location: ' . BASE_URL . '/admin/topics/index.php');
+    header('location: ' . BASE_URL . '/admin/expenses/index.php');
     exit();
 }
 
 
-if (isset($_POST['update-topic'])) {
+if (isset($_POST['update-expense'])) {
     adminOnly();
-    $errors = validateTopic($_POST);
+    $errors = validateExpense($_POST);
 
     if (count($errors) === 0) { 
         $id = $_POST['id'];
-        unset($_POST['update-topic'], $_POST['id']);
-        $topic_id = update($table, $id, $_POST);
-        $_SESSION['message'] = 'Topic updated successfully';
+        unset($_POST['update-expense'], $_POST['id']);
+        $expense_id = update($table, $id, $_POST);
+        $_SESSION['message'] = 'Expense updated successfully';
         $_SESSION['type'] = 'success';
-        header('location: ' . BASE_URL . '/admin/topics/index.php');
+        header('location: ' . BASE_URL . '/admin/expenses/index.php');
         exit();
     } else {
         $id = $_POST['id'];
         $name = $_POST['name'];
+        $amount = $_POST['amount'];
+        $category = $_POST['category'];
+        $account = $_POST['account'];
+        $date = $_POST['date'];
         $description = $_POST['description'];
     }
 
