92cedd6b3a5ddba1d0aa00bd27f80b3c4ae766a8
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-09 21:51:20 -0700

    m

--- app/database/connect.php
+++ app/database/connect.php
@@ -6,5 +6,7 @@
     $conn=mysqli_connect($servername,$username,$password,"$dbname");
       if(!$conn){
           die('Could not Connect MySql Server:' .mysql_error());
+        } else {
+            echo "Success";
         }
 ?>
