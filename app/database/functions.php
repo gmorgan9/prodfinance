1991e91532cb923909562d73c6e519b0d52f386b
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-09 21:31:01 -0700

    m

--- app/database/functions.php
+++ app/database/functions.php
@@ -1,6 +1,5 @@
 <?php 
 session_start();
-require('connect.php');
 
 // connect to database update
 $db = mysqli_connect('localhost', 'gmorg', 'gmorgpass', 'finance');
@@ -125,40 +124,40 @@ function delete($table, $id)
     return $stmt->affected_rows;
 }
 
-function getPublishedPosts()
-{
-    global $db;
-    $sql = "SELECT p.*, u.username FROM posts AS p JOIN users AS u ON p.user_id=u.id WHERE p.published=?";
+// function getPublishedPosts()
+// {
+//     global $db;
+//     $sql = "SELECT p.*, u.username FROM posts AS p JOIN users AS u ON p.user_id=u.id WHERE p.published=?";
 
-    $stmt = executeQuery($sql, ['published' => 1]);
-    $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-    return $records;
-}
+//     $stmt = executeQuery($sql, ['published' => 1]);
+//     $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+//     return $records;
+// }
 
-function getPostsByTopicId($topic_id)
-{
-    global $db;
-    $sql = "SELECT p.*, u.username FROM posts AS p JOIN users AS u ON p.user_id=u.id WHERE p.published=? AND topic_id=?";
+// function getPostsByTopicId($topic_id)
+// {
+//     global $db;
+//     $sql = "SELECT p.*, u.username FROM posts AS p JOIN users AS u ON p.user_id=u.id WHERE p.published=? AND topic_id=?";
 
-    $stmt = executeQuery($sql, ['published' => 1, 'topic_id' => $topic_id]);
-    $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-    return $records;
-}
+//     $stmt = executeQuery($sql, ['published' => 1, 'topic_id' => $topic_id]);
+//     $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+//     return $records;
+// }
 
-function searchPosts($term)
-{
-    $match = '%' . $term . '%';
-    global $conn;
-    $sql = "SELECT 
-                p.*, u.username 
-            FROM posts AS p 
-            JOIN users AS u 
-            ON p.user_id=u.id 
-            WHERE p.published=?
-            AND p.title LIKE ? OR p.body LIKE ?";
+// function searchPosts($term)
+// {
+//     $match = '%' . $term . '%';
+//     global $conn;
+//     $sql = "SELECT 
+//                 p.*, u.username 
+//             FROM posts AS p 
+//             JOIN users AS u 
+//             ON p.user_id=u.id 
+//             WHERE p.published=?
+//             AND p.title LIKE ? OR p.body LIKE ?";
 
 
-    $stmt = executeQuery($sql, ['published' => 1, 'title' => $match, 'body' => $match]);
-    $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-    return $records;
-}
+//     $stmt = executeQuery($sql, ['published' => 1, 'title' => $match, 'body' => $match]);
+//     $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+//     return $records;
+// }
