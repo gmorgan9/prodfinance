67742ce6eea9273cf158a79f4bdff97aeac3430b
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-09 23:58:08 -0700

    m

--- app/includes/adminSidebar.php
+++ app/includes/adminSidebar.php
@@ -6,33 +6,5 @@
         <li><a href="/admin/budget/index.php">Manage Budget</a></li>
         <li><a href="/admin/categories/index.php">Manage Categories</a></li>
         <li><a href="/admin/accounts/index.php">Manage Accounts</a></li>
-
-        
     </ul>
 </div>
-
-
-<style>
-
-
-
-</style>
-
-<script>
-
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
-
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
-    }
-  });
-}
-
-</script>
