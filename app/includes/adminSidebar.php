de4d2a91a2e7205732155617ebc46a13ff413b9b
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-09 23:46:32 -0700

    m

--- app/includes/adminSidebar.php
+++ app/includes/adminSidebar.php
@@ -4,8 +4,8 @@
         <li><a href="/admin/expenses/index.php">Manage Expenses</a></li>
         <li><a href="/admin/income/index.php">Manage Income</a></li>
         <li><a href="/admin/budget/index.php">Manage Budget</a></li>
-        <li><a href="/admin/budget/index.php">Manage Categories</a></li>
-        <li><a href="/admin/budget/index.php">Manage Accounts</a></li>
+        <li><a href="/admin/categories/index.php">Manage Categories</a></li>
+        <li><a href="/admin/accounts/index.php">Manage Accounts</a></li>
 
         
     </ul>
