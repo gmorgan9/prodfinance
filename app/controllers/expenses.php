b92fab544f763869967a27e105d06f8ce92cb228
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-10 14:13:28 -0700

    m

--- app/controllers/expenses.php
+++ app/controllers/expenses.php
@@ -37,41 +37,47 @@ if (isset($_POST['add-expense'])) {
 }
 
 
-// if (isset($_GET['id'])) {
-//     $id = $_GET['id'];
-//     $topic = selectOne($table, ['id' => $id]);
-//     $id = $topic['id'];
-//     $name = $topic['name'];
-//     $description = $topic['description'];
-// }
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $expense = selectOne($table, ['id' => $id]);
+    $id = $expense['id'];
+    $name = $expense['name'];
+    $amount = $expense['amount'];
+    $category = $expense['category'];
+    $date = $expense['date'];
+    $description = $expense['description'];
+}
 
-// if (isset($_GET['del_id'])) {
-//     adminOnly();
-//     $id = $_GET['del_id'];
-//     $count = delete($table, $id);
-//     $_SESSION['message'] = 'Topic deleted successfully';
-//     $_SESSION['type'] = 'success';
-//     header('location: ' . BASE_URL . '/admin/topics/index.php');
-//     exit();
-// }
+if (isset($_GET['del_id'])) {
+    adminOnly();
+    $id = $_GET['del_id'];
+    $count = delete($table, $id);
+    $_SESSION['message'] = 'Expense deleted successfully';
+    $_SESSION['type'] = 'success';
+    header('location: ' . BASE_URL . '/admin/expenses/index.php');
+    exit();
+}
 
 
-// if (isset($_POST['update-topic'])) {
-//     adminOnly();
-//     $errors = validateTopic($_POST);
+if (isset($_POST['update-expense'])) {
+    adminOnly();
+    $errors;
 
-//     if (count($errors) === 0) { 
-//         $id = $_POST['id'];
-//         unset($_POST['update-topic'], $_POST['id']);
-//         $topic_id = update($table, $id, $_POST);
-//         $_SESSION['message'] = 'Topic updated successfully';
-//         $_SESSION['type'] = 'success';
-//         header('location: ' . BASE_URL . '/admin/topics/index.php');
-//         exit();
-//     } else {
-//         $id = $_POST['id'];
-//         $name = $_POST['name'];
-//         $description = $_POST['description'];
-//     }
+    if (count($errors) === 0) { 
+        $id = $_POST['id'];
+        unset($_POST['update-expense'], $_POST['id']);
+        $expense_id = update($table, $id, $_POST);
+        $_SESSION['message'] = 'Expense updated successfully';
+        $_SESSION['type'] = 'success';
+        header('location: ' . BASE_URL . '/admin/expenses/index.php');
+        exit();
+    } else {
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $amount = $_POST['amount'];
+        $category = $_POST['category'];
+        $date = $_POST['date'];
+        $description = $_POST['description'];
+    }
 
-// }
+}
