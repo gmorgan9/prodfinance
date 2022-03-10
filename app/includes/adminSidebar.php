cce1e3ac4be02b9dfcf905e8d568a488047deb60
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-09 23:33:49 -0700

    m

--- app/includes/adminSidebar.php
+++ app/includes/adminSidebar.php
@@ -4,19 +4,15 @@
         <li><a href="/admin/expenses/index.php">Manage Expenses</a></li>
         <li><a href="/admin/income/index.php">Manage Income</a></li>
         <li><a href="/admin/budget/index.php">Manage Budget</a></li>
-        <ul class="lvl2">
-            <li><a href="/admin/income/index.php">Manage Categories</a></li>
-            <li><a href="/admin/budget/index.php">Manage Accounts</a></li>
-        </ul>
+        
+        <button class="dropdown-btn">Settings
+            <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-container">
+            <li><a href="#">Manage Categories</a></li>
+            <li><a href="#">Manage Accounts</a></li>
+            </div>
+
+        
     </ul>
 </div>
-
-<style>
-    ul { padding:0; margin:0;  }
-li { list-style:none; }
-li > ul { display: none; }
-li:hover > ul { display: block; }
-.lvl1 li { margin-right: 10px; display: inline; position:relative; }
-.lvl2 { position: absolute; }
-.lvl2 li { position: relative; }
-</style>
