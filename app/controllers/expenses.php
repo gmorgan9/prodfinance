dfd7b0ff0775429a4c95e0c9fb0053c1a29e09f8
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-09 21:11:04 -0700

    m

--- app/controllers/expenses.php
+++ app/controllers/expenses.php
@@ -18,6 +18,37 @@ $description = "";
 $expenses = selectAll($table);
 
 
+$servername = "localhost";
+$username = "gmorg";
+$password = "gmorgpass";
+$dbname = "finance";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO expenses (name, amount, category, account, date, description)
+VALUES ($name, $amount, $category, $account, $date, $description)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+
+
+
+
+
+
+
+
 if (isset($_POST['add-expense'])) {
     adminOnly();
     $errors = validateExpense($_POST);
