d0bad2d3679021bdfc1512b1b8866f60d4610cf4
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-10 00:09:09 -0700

    m

--- admin/income/create.php
+++ admin/income/create.php
@@ -71,18 +71,35 @@ adminOnly();
 
 
                         <?php
-                                    include '../app/database/connect.php';
-                                    $sql = "SELECT name FROM categories";
-                                    $result = $conn->query($sql);
-                                    if ($result->num_rows > 0) {
-                                        echo "<select name='vehicle_make'>";
-                                        // output data of each row
-                                        while($row = $result->fetch_assoc()) {
-                                          echo "<option value='" . $row['name'] . "'>" . $row['name'] . "</option>";
-                                        }
-                                        echo "</select>";
-                                    } 
-                                    ?>
+    // start of dbcon
+    $servername = "localhost";
+    $username = "gmorg";
+    $password = "gmorgpass";
+    $dbname = "finance";
+
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    //end of dbcon
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+
+    $sql = "SELECT * FROM categories";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+
+        echo "<select name='name'>";
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+          echo "<option value='" . $row['name'] . "'>" . $row['name'] . "</option>";
+        }
+        echo "</select>";
+    } 
+    $conn->close();
+    ?>
 
 
 
