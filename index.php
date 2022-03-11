2209094a8572a212c5c55c87fabeba00eccc9414
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-11 12:10:02 -0700

    m

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ usersOnly();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="icon" type="image/x-icon" href="../assets/images/fav.png?v=<?php echo time(); ?>">
+  <link rel="icon" type="image/x-icon" href="/assets/images/fav.png?v=<?php echo time(); ?>">
 
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
